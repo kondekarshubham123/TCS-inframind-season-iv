b572d3184648f2c919e325f88fc77d23b8eba510
Shubham Kondekar <kondekarshubham123@gmail.com>
2021-02-14 19:16:23 +0530

    [terraform] Instance group added

--- tfinfra/instance_template.tf
+++ tfinfra/instance_template.tf
@@ -39,6 +39,37 @@ resource "google_compute_instance_template" "my-wordpress-template" {
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     email  = google_service_account.default.email
-    scopes = ["sql-admin", "service-control", "service-management", "logging-write", "monitoring-write", "storage-rw", "trace"]
+    scopes = ["sql-admin", "service-control", "service-management", "logging-write", "monitoring-write", "storage-rw"]
+  }
+}
+
+resource "google_compute_health_check" "autohealing" {
+  name                = "autohealing-health-check"
+  check_interval_sec  = 5
+  timeout_sec         = 5
+  healthy_threshold   = 2
+  unhealthy_threshold = 10 # 50 seconds
+
+  http_health_check {
+    request_path = "/healthz"
+    port         = "8080"
+  }
+}
+
+resource "google_compute_instance_group_manager" "wordpress-igm" {
+  name = "wordpress-igm"
+
+  base_instance_name = "wordpress-app"
+  zone               = "us-central1-a"
+
+  version {
+    instance_template  = google_compute_instance_template.my-wordpress-template.id
+  }
+
+  target_size  = 1
+
+  auto_healing_policies {
+    health_check      = google_compute_health_check.autohealing.id
+    initial_delay_sec = 300
   }
 }
